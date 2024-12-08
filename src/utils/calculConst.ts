be7f59238da830a690fdc14857a12dcd20e6ff3f
Pierre ROMAN <pierre.roman.dev@gmail.com>
2024-12-08 15:28:37 +0100

    Separate in files

---
+++ src/utils/calculConst.ts
@@ -0,0 +1,4 @@
+export const OPERATOR = ["+", "-", "*", "/", "(", ")"];
+export const SPECIAL_OPERATOR = ["(", ")"];
+export const OPERANDE = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+export const PRIORITY = ["*/", "+-"];
